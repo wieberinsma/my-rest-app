3ff32c8fa211b142ca28f187eabaef22520f70f6
wieberinsma <wiebe.rinsma@han.nl>
2023-04-19 12:17:30 +0200

    HTML handler for servlet

--- src/main/java/nl/han/resttest/api/config/WebConfig.java
+++ src/main/java/nl/han/resttest/api/config/WebConfig.java
@@ -1,18 +1,35 @@
 package nl.han.resttest.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"nl.han.resttest.domain.user.impl.controller"})
-public class WebConfig implements WebMvcConfigurer
-{
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/**.html", "/**.js")
+                .addResourceLocations("classpath:/pages/", "classpath:/js/");
+    }
+
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setSuffix(".html");
+        return bean;
     }
 }
--- src/main/java/nl/han/resttest/domain/user/impl/controller/HomeController.java
+++ src/main/java/nl/han/resttest/domain/user/impl/controller/HomeController.java
@@ -1,16 +1,13 @@
 package nl.han.resttest.domain.user.impl.controller;
 
-import jdk.jfr.Registered;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping
-    public ResponseEntity<String> home() {
-        return ResponseEntity.status(HttpStatus.OK).body("Hello world!");
+    public String home() {
+        return "home";
     }
 }
